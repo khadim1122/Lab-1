352c304aeff914d968e46f90252b4cc2af6a89ac
Khadim <Khadim@KhAdi__>
2017-03-24 03:29:52 -0700

    lab1

--- src/Laib_One/MiniArry.java
+++ src/Laib_One/MiniArry.java
@@ -1,4 +1,5 @@
 package Laib_One;
+
 import java.util.Scanner;
 
 public class MiniArry {
@@ -10,32 +11,33 @@ public class MiniArry {
         this.a = new int[a];
 
     }
-    public int Max(){
+    public int Max(int[] r){
     
     int mx=a[0];
-    for (int i=1; i<a.length; i++)
-    if(mx<a[i])
-        mx=a[i];
+    for (int i=1; i<r.length; i++)
+    if(mx<r[i])
+        mx=r[i];
     return mx;
     }
 
     public static void main(String[] args) {
-      /*
-        Scanner.scan=new Scanner();
+      
+        Scanner scan=new Scanner(System.in);
       int[] ar;
       ar=new int[10];
+      System.out.println("enter the arry alements");
               for (int c = 0;  c < ar.length; c++) {
-              
+              ar[c]=scan.nextInt();
               
               }
-*/
-        MiniArry x = new MiniArry(25);
+
+        MiniArry x = new MiniArry(2);
         for (int c = 0;  c < x.a.length; c++) {
             x.a[c] = c;
             System.out.println(x.a[c]);
             
-            System.out.println(x.Max());
         }
+        System.out.println(x.Max(ar));
 
     }
 
